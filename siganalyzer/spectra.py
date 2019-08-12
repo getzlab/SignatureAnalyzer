04438e3541c769f9ed983bd4b348a5000fa40ac4
jcha <jcha@broadinstitute.org>
2019-08-12 14:29:46 -0400

    Converted G and T ref-base SNPs to their complements and changed error message

--- siganalyzer/spectra.py
+++ siganalyzer/spectra.py
@@ -7,13 +7,12 @@ context96 = dict(zip(map(''.join, itertools.chain(_acontext, _ccontext)), range(
 
 def get_spectra_from_maf(maf):
     """
-    Attaches context to maf and gets counts of contexts for each sample
+    Attaches context categories to maf and gets counts of contexts for each sample
     Args:
         maf: Pandas DataFrame of maf
-        Should be preprocessed to convert SNPs with G and T ref-bases to their complements
 
     Returns:
-        Pandas DataFrame of maf with context attached
+        Pandas DataFrame of maf with context category attached
         Pandas DataFrame of counts with samples as columns and context as rows
     """
     maf = maf.copy()
@@ -28,16 +27,14 @@ def get_spectra_from_maf(maf):
     context = maf['ref_context'].str.upper()
     n_context = context.str.len()
     mid = n_context // 2
-    contig = pd.Series([r + a + c[m - 1] + c[m + 1] for r, a, c, m in zip(ref, alt, context, mid)], index=maf.index)
+    complements = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
+    contig = pd.Series([r + a + c[m - 1] + c[m + 1] if r in 'AC'
+                        else complements[r] + complements[a] + complements[c[m - 1]] + complements[c[m + 1]]
+                        for r, a, c, m in zip(ref, alt, context, mid)], index=maf.index)
     try:
         maf['context96.num'] = contig.apply(context96.__getitem__)
     except KeyError as e:
-        message = str(e).strip('\'')
-        if message[0] in 'GT':
-            raise KeyError('Complement error')
-        elif message[1] == message[0]:
-            raise KeyError('Alt equals ref')
-        raise KeyError('Unusual context')
+        raise KeyError('Unusual context: ' + str(e))
     maf['context96.word'] = contig
     spectra = maf.groupby(['context96.num', 'sample']).size().unstack().fillna(0).astype(int)
     return maf, spectra

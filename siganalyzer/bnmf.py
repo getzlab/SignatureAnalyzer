3a961739c9a301346db13878bed7fa0e91178fd8
Shankara <sanand@broadinstitute.org>
2019-09-16 22:23:53 +0000

    fixes to siganalyzer-gpu source code for cpu/gpu compat and print

--- siganalyzer/bnmf.py
+++ siganalyzer/bnmf.py
@@ -14,99 +14,110 @@ from ARD_NMF import ARD_NMF, run_method_engine
 from .utils import compute_phi, transfer_weights, select_signatures, select_markers
 
 # ---------------------------------
-# NMF Wrapper
+# ARD - NMF Wrapper
 # ---------------------------------
-def ardnmf(X: pd.DataFrame, \
-           K0: Union[int, None] = None, \
-           objective: str = 'poisson', \
-           max_iter: int = 10000, \
-           del_: int = 1, \
-           tolerance: float = 1e-6, \
-           phi: Union[float, None] = None, \
-           a: float = 10.0, \
-           b: Union[float, None] = None , \
-           prior_on_W: str = 'L1', \
-           prior_on_H: str = 'L1', \
-           report_frequency: int = 100, \
-           active_thresh: float = 1e-5, \
-           cut_norm: float = 0.5, \
-           cut_diff: float = 1.0
-       ) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame):
-        """
-        Wrapper for ARD-NMF. Wraps core algorithm from:
-        https://github.com/broadinstitute/SignatureAnalyzer-GPU
-        ------------------------------------------------------------------------
-        Args:
-            * X: input matrix (features x samples)
-            * K0: starting number of latent components
-            * objective: objective function for optimizaiton
-            * max_iter: maximum number of iterations for algorithm
-            * del_: n/a
-            * tolerance: stop point for optimization
-            * phi: dispersion parameter
-            * a: shape parameter
-            * b: shape parameter
-            * prior_on_W: L1 or L2
-            * prior_on_H: L1 or L2
-            * report_frequency: how often to print stats
-            * parameters: parameters file
-            * cut_norm: min normalized value for mean signature
-            * cut_diff: difference between mean signature and rest of signatures
-                for marker selction
-            * active_thresh: threshold for a latent component's impact on
-                signature if the latent factor is less than this, it does not contribute
+def ardnmf(
+    X: pd.DataFrame,
+    K0: Union[int, None] = None,
+    objective: str = 'poisson',
+    max_iter: int = 10000,
+    del_: int = 1,
+    tolerance: float = 1e-6,
+    phi: float = 1.0,
+    a: float = 10.0,
+    b: Union[float, None] = None ,
+    prior_on_W: str = 'L1',
+    prior_on_H: str = 'L1',
+    report_freq: int = 100,
+    active_thresh: float = 1e-5,
+    cut_norm: float = 0.5,
+    cut_diff: float = 1.0,
+    cuda_int: Union[int, None] = 0,
+    verbose: bool = True
+    ) -> (pd.DataFrame, pd.DataFrame, pd.DataFrame, pd.DataFrame):
+    """
+    Wrapper for ARD-NMF. Wraps GPU implementaiton from:
+    https://github.com/broadinstitute/SignatureAnalyzer-GPU
+    ------------------------------------------------------------------------
+    Args:
+        * X: input matrix (features x samples)
+        * K0: starting number of latent components
+        * objective: objective function for optimizaiton
+        * max_iter: maximum number of iterations for algorithm
+        * del_: n/a
+        * tolerance: stop point for optimization
+        * phi: dispersion parameter
+        * a: shape parameter
+        * b: shape parameter
+        * prior_on_W: L1 or L2
+        * prior_on_H: L1 or L2
+        * report_freq: how often to print stats
+        * active_thresh: threshold for a latent component's impact on
+            signature if the latent factor is less than this, it does not contribute
+        * cut_norm: min normalized value for mean signature
+            (used in post-processing)
+        * cut_diff: difference between mean signature and rest of signatures
+            for marker selction
+            (used in post-processing)
+        * cuda_int: GPU to use. Defaults to 0. If "None" or if no GPU available,
+            will perform decomposition using CPU.
+        * verbose: verbose reporting of algorithm convergence
 
-        Returns:
-            * H: (samples x K)
-            * W: (K x features)
-            * markers
-            * signatures
-        """
+    Returns:
+        * H: (samples x K)
+        * W: (K x features)
+        * markers
+        * signatures
+    """
+    assert objective in ('poisson','gaussian'), \
+        "Unable to use {}; specify either poisson or gaussian objective.".format(objective)
 
-        assert objective in ['poisson','gaussian'], \
-            "Unable to use {}; specify either poisson or gaussian objective.".format(objective)
+    if objective == 'poisson': Beta = 1
+    if objective == 'gaussian': Beta = 2
 
-        if objective == 'poisson': Beta = 1
-        if objective == 'gaussian': Beta = 2
+    assert prior_on_W in ('L1','L2'), \
+        "Unable to use {}; use either L1 or L2 prior on W.".format(prior_on_W)
+    assert prior_on_H in ('L1','L2'), \
+        "Unable to use {}; use either L1 or L2 prior on H.".format(prior_on_H)
 
-        assert prior_on_W in ['L1','L2'], \
-            "Unable to use {}; use either L1 or L2 prior on W.".format(prior_on_W)
-        assert prior_on_H in ['L1','L2'], \
-            "Unable to use {}; use either L1 or L2 prior on H.".format(prior_on_H)
+    # TODO: remove
+    # if phi is None:
+    #     phi = compute_phi(np.mean(X.values), np.var(X.values), Beta)
+    #     print("Using computed phi of {}".format(phi))
 
-        if phi is None:
-            phi = compute_phi(np.mean(X.values), np.var(X.values), Beta)
-            print("Using computed phi of {}".format(phi))
+    # ---------------------------------
+    # Load data into tensors
+    # ---------------------------------
+    data = ARD_NMF(X, objective, verbose=verbose)
+    channel_names = data.channel_names
+    sample_names = data.sample_names
 
-        # ---------------------------------
-        # Load data into tensors
-        # ---------------------------------
-        data = ARD_NMF(X, objective)
-        channel_names = data.channel_names
-        sample_names = data.sample_names
+    # ---------------------------------
+    # Run NMF
+    # ---------------------------------
+    W, H, cost = run_method_engine(
+        data, \
+        a, \
+        phi, \
+        b, \
+        Beta, \
+        prior_on_W, \
+        prior_on_H, \
+        K0, \
+        tolerance, \
+        max_iter, \
+        report_freq=report_freq, \
+        active_thresh=active_thresh, \
+        cuda_int=cuda_int, \
+        verbose=verbose
+    )
 
-        # ---------------------------------
-        # Run NMF
-        # ---------------------------------
-        W, H, cost = run_method_engine(
-            data, \
-            a, \
-            phi, \
-            b, \
-            Beta, \
-            prior_on_W, \
-            prior_on_H, \
-            K0, \
-            tolerance, \
-            max_iter \
-        )
+    W,H,nsig = transfer_weights(W, H, active_thresh=active_thresh)
+    sig_names = [str(i) for i in range(1,nsig+1)]
+    W = pd.DataFrame(data=W, index=channel_names, columns=sig_names)
+    H = pd.DataFrame(data=H, index=sig_names, columns=sample_names)
 
-        W,H,nsig = transfer_weights(W, H, active_thresh=active_thresh)
-        sig_names = [str(i) for i in range(1,nsig+1)]
-        W = pd.DataFrame(data=W, index=channel_names, columns=sig_names)
-        H = pd.DataFrame(data=H, index=sig_names, columns=sample_names)
+    W,H = select_signatures(W,H)
+    markers, signatures = select_markers(X, W, H, cut_norm=cut_norm, cut_diff=cut_diff)
 
-        W,H = select_signatures(W,H)
-        markers, signatures = select_markers(X, W, H, cut_norm=cut_norm, cut_diff=cut_diff)
-
-        return H,W,markers,signatures
+    return H,W,markers,signatures

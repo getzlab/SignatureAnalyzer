e1f4287b06d3508195f6ce414e44aa504c73de51
Shankara <sanand@broadinstitute.org>
2019-09-30 21:15:36 +0000

    plotting api

--- siganalyzer/__init__.py
+++ siganalyzer/__init__.py
@@ -1,2 +1,5 @@
 from .bnmf import ardnmf
 from .utils import postprocess_msigs
+
+from . import plotting as pl
+from .consensus import consensus_cluster
---
+++ siganalyzer/plotting/__init__.py
@@ -0,0 +1,10 @@
+"""
+Signature Analyzer plotting API.
+"""
+from ._rna import marker_heatmap
+
+from ._muts import signature_barplot
+from ._muts import stacked_bar
+
+from ._nmf import k_dist
+from ._nmf import consensus_matrix

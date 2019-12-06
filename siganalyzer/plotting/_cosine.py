25be930a88c2ed5359520e600a83ac161ea3a886
jcha <jcha@broadinstitute.org>
2019-12-06 14:49:49 -0500

    Adjusted cosine similarity plot figsize so colorbar is constant

--- siganalyzer/plotting/_cosine.py
+++ siganalyzer/plotting/_cosine.py
@@ -22,7 +22,7 @@ def cosine_similarity_plot(cosine_similarity_matrix: pd.DataFrame):
     sig_order = scipy.cluster.hierarchy.dendrogram(linkage_mat, no_plot=True)['leaves']
     sorted_cosine_similarity_matrix = cosine_similarity_matrix.iloc[sig_order]
 
-    fig, (ax, cbar_ax) = plt.subplots(figsize=(cosine_similarity_matrix.shape[1], cosine_similarity_matrix.shape[0] / 3),
+    fig, (ax, cbar_ax) = plt.subplots(figsize=(cosine_similarity_matrix.shape[1] + .5, cosine_similarity_matrix.shape[0] / 3),
                                       ncols=2, gridspec_kw={'width_ratios': [cosine_similarity_matrix.shape[1] * 2, 1]})
     sns.heatmap(sorted_cosine_similarity_matrix, vmin=.75, vmax=1, ax=ax, cbar_ax=cbar_ax, cmap='viridis', annot=True)
     ax.set_yticks(np.arange(cosine_similarity_matrix.shape[0]) + .5)
